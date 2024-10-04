25970549347c99ff827d2fc54a5dccb67baaf81b
tavukkdoner <166294260+tavukkdoner@users.noreply.github.com>
2024-10-04 20:21:37 +0300

    envsetup: Remove nobody@android-build

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -911,5 +911,5 @@ function fixup_common_out_dir() {
 export USE_THINLTO_CACHE=true
 
 # Override host metadata to make builds more reproducible and avoid leaking info
-export BUILD_USERNAME=nobody
-export BUILD_HOSTNAME=android-build
+# export BUILD_USERNAME=nobody
+# export BUILD_HOSTNAME=android-build
